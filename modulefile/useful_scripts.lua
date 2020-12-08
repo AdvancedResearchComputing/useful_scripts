686b3997e40d26f75f96f8783dc13989005927c9
Justin Krometis <jkrometis@vt.edu>
2020-12-08 12:54:39 -0500

    add modulefile

---
+++ modulefile/useful_scripts.lua
@@ -0,0 +1,17 @@
+help([==[
+
+Description
+===========
+This module contains useful scripts for ARC users
+
+
+More information
+================
+ - Homepage: https://github.com/AdvancedResearchComputing/useful_scripts
+]==])
+
+whatis([==[Description: Useful scripts for ARC users]==])
+whatis([==[URL: https://github.com/AdvancedResearchComputing/useful_scripts]==])
+
+prepend_path("PATH", "/apps/useful_scripts/bin")
+
